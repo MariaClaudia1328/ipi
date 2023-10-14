9d28f2c00acda9bfd59195be14d73616df6f1ab6
Maria Claudia <mariaclaudia8.0@hotmail.com>
2023-10-14 15:43:30 -0300

    implementa aumento de imagem em multiplo de 2 com 2 formas de preencher pixels

--- trabalho1/src/LER_YUV.py
+++ trabalho1/src/LER_YUV.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 
 def LER_YUV(filePath, width, height, frameNumber):
     
@@ -12,43 +13,120 @@ def LER_YUV(filePath, width, height, frameNumber):
         bytesToSkip = frameNumber * width * height
         file.seek(bytesToSkip, 0 )
 
-        yuvData = file.read(frameSize)
+        yuvData = np.fromfile(file, dtype=np.uint8, count = frameSize)
 
         ySize = width * height
         uvSize = ySize // 4
 
-        Y = yuvData[:ySize]
-        U = yuvData[ySize:ySize+uvSize]
-        V = yuvData[ySize + uvSize:]
-
-        Y = np.frombuffer(Y, dtype=np.uint8).reshape(height, width)
-        U = np.frombuffer(U, dtype=np.uint8).reshape(height//2, width//2)
-        V = np.frombuffer(V, dtype=np.uint8).reshape(height//2, width//2)
+        Y = yuvData[:ySize].reshape(height, width)
+        U = yuvData[ySize:ySize+uvSize].reshape(height//2, width//2)
+        V = yuvData[ySize + uvSize:].reshape(height//2, width//2)
 
     return [Y,U,V]
 
-[Y,U,V] = LER_YUV("foreman.yuv", 358, 288, 10)
 
+def resize2hw (image, newSize):
 
-def resize2hw (image):
-    newImageHeight = image.height*2
-    newImageWidth = image.width*2
+    imageHeight = image.shape[0]
+    imageWidth = image.shape[1]
 
-    newImage = np.zeros((newImageWidth, newImageHeight))
+    newImageHeight = imageHeight*2*newSize
+    newImageWidth = imageWidth*2*newSize
 
-    # copia a primeira coluna pra newImage 
-        # pula pra coluna de newImage + 2
+    newImage = np.zeros((newImageHeight, newImageWidth), dtype=np.uint8)
+
+    l = 0
+    m = 0
+    for i in range(0,imageHeight):
+        for j in range(0,imageWidth):
+            newImage[l][m] = image[i][j]
+            m += 2
+        l += 2
+        m = 0
+        
+    return newImage
+
+def fillWithImmediates (imageIn):
+    image = imageIn.copy()
+    imageHeight = image.shape[0]
+    imageWidth = image.shape[1]
+
+    for i in range(0,imageHeight):
+        for j in range(0,imageWidth):
+            if(np.all(image[i][j] != 0)): continue
+            
+            if(i != 0): 
+                image[i][j] = image[i-1][j]
+            else:
+                image[i][j] = image[i][j-1]
+    return image
+
+def fillWithAvarage (imageIn):
+    image = imageIn.copy()
+    imageHeight = image.shape[0]
+    imageWidth = image.shape[1]
+
     
-    # copia a primeira linha pra newImage
-        # pula pra linha de newImage + 2
+    for i in range(0,imageHeight):
+        for j in range(0,imageWidth):
+            if(np.all(image[i][j] != 0)): continue
+
+            if(i%2 == 0):
+                if(j+1 >= imageWidth):
+                    image[i][j] = image[i][j-1]
+                else:
+                    image[i][j] = (np.uint16(image[i][j-1]) + np.uint16(image[i][j+1] )) / 2
+            elif(j%2 == 0):
+                if(i+1 >= imageHeight):
+                    image[i][j] = image[i-1][j]
+                else:
+                    image[i][j] = (np.uint16(image[i-1][j]) + np.uint16(image[i+1][j] )) / 2
+            else:
+                if(i+1 >= imageHeight and j+1 >= imageWidth):
+                    image[i][j] = image[i-1][j-1]
+                elif(i+1 >= imageHeight):
+                    image[i][j] = (np.uint16(image[i-1][j-1]) + np.uint16(image[i-1][j+1] )) / 2
+                elif(j+1 >= imageWidth):
+                    image[i][j] = (np.uint16(image[i-1][j-1]) + np.uint16(image[i+1][j-1] )) / 2
+                else:
+                    image[i][j] = (np.uint16(image[i-1][j-1]) + np.uint16(image[i+1][j+1]) + np.uint16(image[i-1][j+1]) + np.uint16(image[i+1][j-1])) / 4
+    return image
+
+[Y,U,V] = LER_YUV("foreman.yuv", 352, 288, 15)
 
 
-def fillWithImmediates (image):
-    # percorre todo newImage e subtitui os pixels pretos (valor = 0)
-    return
+originalY = Y
+originalU = U
+originalV = V
 
-def fillWithAvarage (image):
-    return
+originalY = resize2hw(originalY, 1)
+originalU = resize2hw(originalU, 1)
+originalV = resize2hw(originalV, 1)
 
+originalY = fillWithAvarage(originalY)
+originalU = fillWithAvarage(originalU)
+originalV = fillWithAvarage(originalV)
 
+originalU = np.repeat(originalU, 2, axis=0)
+originalU = np.repeat(originalU, 2, axis=1)
+originalV = np.repeat(originalV, 2, axis=0)
+originalV = np.repeat(originalV, 2, axis=1)
+
+yuvOriginal = np.stack((originalY,originalV,originalU), axis = -1)
+bgr_image = cv2.cvtColor(yuvOriginal, cv2.COLOR_YCR_CB2BGR)
+cv2.imshow('YUV Original Image', bgr_image)
+
+newU = resize2hw(U, 1)
+newV = resize2hw(V, 1)
+
+newU = fillWithAvarage(newU)
+newV = fillWithAvarage(newV)
+
+mergedYUV = cv2.merge([Y, newV, newU])
+print(mergedYUV.shape)
+bgr_image = cv2.cvtColor(mergedYUV, cv2.COLOR_YCrCb2BGR)
+
+cv2.imshow('YUV Image', bgr_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
