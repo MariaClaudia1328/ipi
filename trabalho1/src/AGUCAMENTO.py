e0b2cc0cb7be71fcace43812b0160c1515d5a976
Maria Claudia <mariaclaudia8.0@hotmail.com>
2023-10-15 12:36:23 -0300

    implementa filtro gaussiano e outro tipo de filtro laplaciano

--- trabalho1/src/AGUCAMENTO.py
+++ trabalho1/src/AGUCAMENTO.py
@@ -12,7 +12,7 @@ def sharpening(image, filterType):
         laplacianKernel = np.array([[-1,-1,-1],
                                     [-1,8,-1],
                                     [-1,-1,-1]])
-    elif(filterType == "4"):
+    elif(filterType == "-4"):
         laplacianKernel = np.array([[0,1,0],
                                     [1,-4,1],
                                     [0,1,0]])
@@ -23,17 +23,32 @@ def sharpening(image, filterType):
     else:
         print("O tipo de filtro laplaciano pode ser +/-8 ou +/-4")
         exit(1)
-        
+
     image = cv2.imread(image,cv2.IMREAD_GRAYSCALE)
     laplacianOutput = cv2.filter2D(image, cv2.CV_64F, laplacianKernel)
     
     laplacianOutput = cv2.convertScaleAbs(laplacianOutput)
     laplacianOutput = cv2.add(laplacianOutput, 128)
 
-    cv2.imwrite("../assets/questao2/questao2-1/originalImage.jpg", image)
-    cv2.imwrite(f'../assets/questao2/questao2-1/laplacianFilteredImage_{filterType}.jpg', laplacianOutput)
+    if(filterType != "4" and filterType != "-4"):
+        cv2.imwrite("../assets/questao2/questao2-1/originalImage.jpg", image)
+        cv2.imwrite(f'../assets/questao2/questao2-1/laplacianFilteredImage_{filterType}.jpg', laplacianOutput)
     
+    blur = cv2.GaussianBlur(image, (3,3), 0.5)
+    laplacianOutput = cv2.filter2D(blur, cv2.CV_64F, laplacianKernel)
+    
+    laplacianOutput = cv2.convertScaleAbs(laplacianOutput)
+    laplacianOutput = cv2.add(laplacianOutput, 128)
 
+    cv2.imwrite(f"../assets/questao2/questao2-2/gaussianBlurAndLaplacian_{filterType}.jpg", laplacianOutput)
+
+    blur = cv2.GaussianBlur(image, (3,3), 1)
+    laplacianOutput = cv2.filter2D(blur, cv2.CV_64F, laplacianKernel)
+    
+    laplacianOutput = cv2.convertScaleAbs(laplacianOutput)
+    laplacianOutput = cv2.add(laplacianOutput, 128)
+
+    cv2.imwrite(f"../assets/questao2/questao2-3/gaussianBlurAndLaplacian_{filterType}.jpg", laplacianOutput)
 
     return
 
@@ -41,4 +56,5 @@ if(len(sys.argv) < 3):
     print("Executar programa com nome do arquivo e qual o tipo do filtro Laplaciano.")
     exit(1)
 
+# Coleta de resultados
 sharpening(sys.argv[1], sys.argv[2])
