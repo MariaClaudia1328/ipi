39a4b401cc936a9f467ac17a9d407e5f45283a4c
Maria Claudia <mariaclaudia8.0@hotmail.com>
2023-10-15 12:10:24 -0300

    implementacao da questao 2-1

--- trabalho1/src/AGUCAMENTO.py
+++ trabalho1/src/AGUCAMENTO.py
@@ -4,6 +4,36 @@ import sys
 
 def sharpening(image, filterType):
 
+    if(filterType == "-8"):
+        laplacianKernel = np.array([[1,1,1],
+                                    [1,-8,1],
+                                    [1,1,1]])
+    elif(filterType == "8"):
+        laplacianKernel = np.array([[-1,-1,-1],
+                                    [-1,8,-1],
+                                    [-1,-1,-1]])
+    elif(filterType == "4"):
+        laplacianKernel = np.array([[0,1,0],
+                                    [1,-4,1],
+                                    [0,1,0]])
+    elif(filterType == "4"):
+        laplacianKernel = np.array([[0,-1,0],
+                                    [-1,4,-1],
+                                    [0,-1,0]])
+    else:
+        print("O tipo de filtro laplaciano pode ser +/-8 ou +/-4")
+        exit(1)
+        
+    image = cv2.imread(image,cv2.IMREAD_GRAYSCALE)
+    laplacianOutput = cv2.filter2D(image, cv2.CV_64F, laplacianKernel)
+    
+    laplacianOutput = cv2.convertScaleAbs(laplacianOutput)
+    laplacianOutput = cv2.add(laplacianOutput, 128)
+
+    cv2.imwrite("../assets/questao2/questao2-1/originalImage.jpg", image)
+    cv2.imwrite(f'../assets/questao2/questao2-1/laplacianFilteredImage_{filterType}.jpg', laplacianOutput)
+    
+
 
     return
 
